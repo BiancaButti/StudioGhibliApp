8116dfe9c4cc8c16fcc93e4451ecfeb4d932e405
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2025-05-19 08:26:42 -0300

    fix: remove code case error

--- StudioGhibliApp/StudioGhibliApp/ViewModel/MovieListViewModel.swift
+++ StudioGhibliApp/StudioGhibliApp/ViewModel/MovieListViewModel.swift
@@ -11,35 +11,29 @@ final class MovieListViewModel {
     
     func fetchMovies() {
         onLoadingStateChange?(true)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.movies = []
-            self.onLoadingStateChange?(false)
-            self.onMoviesUpdated?()
+        apiService.fetchMoviesService { [weak self] result in
+            DispatchQueue.main.async {
+                self?.onLoadingStateChange?(false)
+                switch result {
+                case .success(let movies):
+                    self?.movies = movies.map {
+                        MovieViewModel(
+                            id: $0.id,
+                            title: $0.title,
+                            originalTitle: $0.originalTitle,
+                            image: $0.image ?? "",
+                            description: $0.description,
+                            producer: $0.producer,
+                            releaseDate: $0.releaseDate,
+                            runningTime: $0.runningTime,
+                            ratingScore: $0.ratingScore,
+                            people: $0.people)
+                    }
+                    self?.onMoviesUpdated?()
+                case .failure(_):
+                    self?.onError?("Occur an error when loading all movies. Try again")
+                }
+            }
         }
-//        apiService.fetchMoviesService { [weak self] result in
-//            DispatchQueue.main.async {
-//                self?.onLoadingStateChange?(false)
-//                switch result {
-//                case .success(let movies):
-//                    self?.movies = movies.map {
-//                        MovieViewModel(
-//                            id: $0.id,
-//                            title: $0.title,
-//                            originalTitle: $0.originalTitle,
-//                            image: $0.image ?? "",
-//                            description: $0.description,
-//                            producer: $0.producer,
-//                            releaseDate: $0.releaseDate,
-//                            runningTime: $0.runningTime,
-//                            ratingScore: $0.ratingScore,
-//                            people: $0.people)
-//                    }
-//                    self?.onMoviesUpdated?()
-//                case .failure(_):
-//                    self?.onError?("Occur an error when loading all movies. Try again")
-//                }
-//            }
-//        }
     }
 }
